66eb4a96fad2c66224c2ce00e884f60e83c99116
Patrick van Staveren <trick@vanstaveren.us>
2020-08-09 00:04:56 -0500

    add calibration magic factor of 153 based on my experience with SCT-013, 22ohm burden, WeMos D1 Mini 3.3v, and coffee/kettle tests. change voltage to 120 although this is estimated, and set cycle time to 1676 per the sampling rate in the comments. added a bunch of notes which are incoherent.

--- prometheus-esp8266-current-monitor.ino
+++ prometheus-esp8266-current-monitor.ino
@@ -6,14 +6,45 @@ void setup()
 {
   Serial.begin(9600);
 
-  emon1.current(0, 111.1);             // Current: input pin, calibration.
-  //emon1.current(0, 18.0);             // Current: input pin, calibration.
+  //emon1.current(0, 111.1);             // Current: input pin, calibration.
+  emon1.current(0, 153);             // Current: input pin, calibration.
 }
 
 void loop()
 {
-  double Irms = emon1.calcIrms(1480);  // Calculate Irms only
-  Serial.print(Irms*230.0);           // Apparent power
+  double Irms = emon1.calcIrms(1676);  // Calculate Irms only
+  Serial.print(Irms*120);           // Apparent power
   Serial.print(" ");
   Serial.println(Irms);             // Irms
 }
+
+
+
+/*
+ * 
+ * 
+ * 
+ * 
+
+notes on calibration: 111.1 is "the mains volt required to drive the secondary voltage to 1v DC"
+https://community.openenergymonitor.org/t/calibration-and-parameters-in-emonlib/6855
+
+the calcIrms magic number of 1676 comes from here: https://community.openenergymonitor.org/t/sampling-rate-of-emonlib/4383
+
+The 100A sensor I'm using is SCT013 100A:50ma which is a 2000:1 ratio CT.
+https://learn.openenergymonitor.org/electricity-monitoring/ct-sensors/yhdc-sct-013-000-ct-sensor-report
+
+my burden is 33 ohm, a kludge of 3 22-ohm resistors, in series, two in parallel
+
+secondary current = primary current (11.2A) / ratio (2000) = 0.0056
+input pin voltage = secondary current (0.0056) * 33 = 369.6
+
+...
+current constant = 100 / .05 = 2000
+
+
+
+11,2 I'm seeing 8
+
+
+ */
